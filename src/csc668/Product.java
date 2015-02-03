6373211d2d9d4759db5a36dd76aa9d40baa4b8df
bcmrcool <jamesbond824@yahoo.com>
2015-02-03 14:42:53 -0800

    Update Product.java
    Adding product class

--- src/csc668/Product.java
+++ src/csc668/Product.java
@@ -5,10 +5,24 @@
  */
 package csc668;
 
-/**
- *
- * @author felipesales
- */
-public class Product {
+public class Product 
+{
+    private String upc;
+    private String name;
+    double price;
+    
+    public String getUPC(){
+        return this.upc;
+    }
+    public void setUPC(String newUPC){
+        this.upc=newUPC;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String newName){
+        this.name=newName;
+    }
     
 }
+
