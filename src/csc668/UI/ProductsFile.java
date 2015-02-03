77ac07f8f0af6e8b8bb8de981a9a7b9a166876f5
Felipe Sales <felipesales@sfs-wifi-aruba-dhcp-130-212-156-127.sfsu.edu>
2015-02-03 14:56:57 -0800

    Finish the ProductFile class

--- src/csc668/UI/ProductsFile.java
+++ src/csc668/UI/ProductsFile.java
@@ -1,6 +1,12 @@
 package csc668.UI;
 
 import csc668.Product;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,15 +23,40 @@ public class ProductsFile {
 
     List<Product> products;
     String fileName;
-    
-    public ProductsFile(String fileName){
+
+    public ProductsFile(String fileName) {
         products = new ArrayList<Product>();
         this.fileName = fileName;
     }
-    
-    public List<Product> parse(){
-        
-        
+
+    public List<Product> parse() {
+        BufferedReader buffer = null;
+
+        try {
+
+            String sCurrentLine;
+            Product newProduct = null;
+
+            buffer = new BufferedReader(new FileReader(fileName));
+
+            while ((sCurrentLine = buffer.readLine()) != null) {
+                String[] parts = sCurrentLine.split(";");
+                newProduct = new Product(parts[0], parts[1], Double.parseDouble(parts[2]));
+                
+                products.add(newProduct);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (buffer != null) {
+                    buffer.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+
         return products;
     }
 }
