241e7f93bf0099d6eaaebfda38cad213a275a9b3
bcmrcool <jamesbond824@yahoo.com>
2015-02-03 14:56:47 -0800

    added prodcut getPrice()

--- src/csc668/Product.java
+++ src/csc668/Product.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package csc668;
+
 
 public class Product 
 {
@@ -23,6 +19,8 @@ public class Product
     public void setName(String newName){
         this.name=newName;
     }
+    public double getPrice(){
+        return price;
+    }
     
 }
-
