483b4b2ed47cf42107fb091b7afca8ae31ed3913
mosesjlee <mosesjlee@gmail.com>
2015-02-05 15:37:46 -0800

    added more things to the main class

--- src/csc668/CSC668.java
+++ src/csc668/CSC668.java
@@ -14,83 +14,63 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class CSC668 {
 
- public static void main(String args[])
-{
+    public static void main(String args[])
+    {
+        //Variables tat the entire program needs
+        ProductsFile productDB = new ProductsFile("products.txt");
+        ArrayList <Product> productDBList = productDB.getProducts();
+        ArrayList <Product> custProductList;
+        Transaction t;
+        
+        //Variables if the user reads from order file
+        int hasOrderForm = 1;   //1 to read from a transaction file else to manually input
+        String fileName;
+        TransactionFile orderForm;
+
+        //Variables if the user manually inputs
         String name;
         Payment payType;
-        ArrayList <Product> custProductList;
-        ProductsFile productDB = new ProductsFile("products.txt");
-        ArrayList <Product> productDBList = productDB.parse();
-        int hasOrderForm = 1;   //1 to read from a transaction file else to manually input
-        TransactionFile orderForm;
-        
-	ArrayList <Product> productList1 = new ArrayList();
-        ArrayList <Product> productList2 = new ArrayList();
-        ArrayList <Product> productList3 = new ArrayList();
-
-	for (int i = 0; i < 10; i++)
-	{
-		Product p = new Product("1234", "Toilet", 50.50);
-		Product p1 = new Product("0987", "Sink", 100.00);
-		Product p2 = new Product("7889", "tub", 500.00);
-
-		productList1.add(p);
-		productList2.add(p1);
-		productList3.add(p2);
-	}
-
-
-
-	/*Payment payCredit = new Credit();
-	Payment payCheck = new Check();
-	Payment payCash = new Cash();*/
-
-	Transaction t1 = new Transaction();
-	Transaction t2 = new Transaction();
-	Transaction t3 = new Transaction();
 
         //Greeting and intro
         System.out.println("Welcome to Point of Sale Terminal!");
         System.out.println("Enter your name, the UPC numbers and " + 
                             "your method of payment");
         System.out.println("Afterwards a receipt will be printed for you");
-        System.out.println("Please enter 1 if you have an order form");
-        System.out.println("If not enter 0");
-        Scanner inputScan = new Scanner(System.in);
-                    
-        hasOrderForm = inputScan.nextInt();
-        
-        if(hasOrderForm == 1)
-        {
-           
-        }
-        //User manualy inputs order
-        else
-        {
 
-            System.out.println("Enter your name: ");
-            name = inputScan.next();
-            while(true)
+        //Begin the user interaction
+        while(hasOrderForm >= 0)
+        {
+            System.out.println("Please enter 1 if you have an order form");
+            System.out.println("If not enter 0 or -1 to exit");
+            Scanner inputScan = new Scanner(System.in);
+
+            hasOrderForm = inputScan.nextInt();
+            //Reads the order form
+            if(hasOrderForm == 1)
             {
-                System.out.println("Enter the UPC of your item (Enter -1 to if done): ");
-                int upc = inputScan.nextInt();
-                
+               System.out.println("Enter in the order file name: ");
+               fileName = inputScan.next();
+               try
+               {
+                orderForm = new TransactionFile(fileName);
+               }
+               catch (Exception e){
+                   System.out.println("Invalid File Try Again");
+               }
+            }
+            //User manualy inputs order
+            else
+            {
 
+                System.out.println("Enter your name: ");
+                name = inputScan.next();
+                while(true)
+                {
+                    System.out.println("Enter the UPC of your item (Enter -1 to if done): ");
+                    int upc = inputScan.nextInt();
+                }
             }
         }
-        
-        
-    }
- 
-    /**
-     * Test print method to ensure correct
-     * method functions
-     * @param t
-     */
-     void printTransactionRecords(Transaction t)
-    {
-
-    }
-    
+    }   
 }
 
