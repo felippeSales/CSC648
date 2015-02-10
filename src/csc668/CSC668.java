89988b0d523583d342dc3123dcfbcdc9a3f1755d
mosesjlee <mosesjlee@gmail.com>
2015-02-10 13:07:25 -0800

    made changes to main class

--- src/csc668/CSC668.java
+++ src/csc668/CSC668.java
@@ -34,83 +34,40 @@ public class CSC668 {
 
         //Greeting and intro
         System.out.println("Welcome to Point of Sale Terminal!");
-        System.out.println("Enter your name, the UPC numbers and " + 
-                            "your method of payment");
+        System.out.println("Enter the name of the file with the transactions");
         System.out.println("Afterwards a receipt will be printed for you");
         Scanner inputScan = new Scanner(System.in);
 
         //Begin the user interaction
         while(hasOrderForm >= 0)
         {
-            System.out.println("Please enter 1 if you have an order form");
-            System.out.println("If not enter 0 or -1 to exit: ");
 
-            hasOrderForm = inputScan.nextInt();
+           hasOrderForm = inputScan.nextInt();
             
-            //Reads the order form
-            if(hasOrderForm == 1)
-            {
-               System.out.println("Enter in the order file name: ");
-               fileName = inputScan.next();
-               try
-               {
-                   orderForm = new TransactionFile(fileName, productDBList);
-               
-               }
-               catch (Exception e)
-               {
-                   System.out.println("Invalid File Try Again");
-                   break;
-               }
-               
-               
-               currentTransaction = orderForm.getTransactions();
-               for(Transaction t : currentTransaction)
-               {
-                   System.out.println("Name: " + t.getName());
-                   System.out.println("total price: " + t.getTotalPrice());
-                   if(t.getPayType() instanceof CreditPayment)
-                   {
-                       System.out.println("Paid by credit card");
-                       System.out.println("Credit Number:" + ((CreditPayment )t.getPayType()).getCreditNumber());
-                       System.out.println("Is valid: " + t.getPayType().isValid());
-                   }
-                   else if(t.getPayType() instanceof CheckPayment)
-                   {
-                       System.out.println("Paid by check");
-                       System.out.println("Check Number:" + ((CheckPayment )t.getPayType()).getCheckNumber());
-                       System.out.println("Is valid: " + t.getPayType().isValid());
-                       System.out.println("Amount tendered: " + t.getAmountTendered());
-                       System.out.println("Amount returned: " + t.getAmountReturned());
-                   }
-                   else if(t.getPayType() instanceof CashPayment)
-                   {
-                       System.out.println("Paid by cash");
-                       System.out.println("Amount tendered: " + t.getAmountTendered());
-                       System.out.println("Amount returned: " + t.getAmountReturned());
-                   }
-                   for(Product p : t.getProducts())
-                   {
-                       System.out.println(p.getName());
-                   }
-                   Receipt test = new Receipt(t);
-                   test.WriteFile("ReceiptList.txt");
-                   test.WriteFile(t.getDateAndTime());
-               }
-            }
-            //User manualy inputs order
-            else if(hasOrderForm == 0)
-            {
+           //Reads the order form
+           System.out.println("Enter in the order file name: ");
+           fileName = inputScan.next();
+           try
+           {
+               orderForm = new TransactionFile(fileName, productDBList);
 
-                System.out.println("Enter your name: ");
-                name = inputScan.next();
-                while(true)
-                {
-                    System.out.println("Enter the UPC of your item (Enter -1 to if done): ");
-                    int upc = inputScan.nextInt();
-                    if(upc == -1) break;
-                }
-            }
+           }
+           catch (Exception e)
+           {
+               System.out.println("Invalid File Try Again");
+               break;
+           }
+
+
+           currentTransaction = orderForm.getTransactions();
+
+           System.out.println("Printing receipt");
+           for(Transaction t : currentTransaction)
+           {
+               Receipt test = new Receipt(t);
+               test.WriteFile("ReceiptList.txt");
+               test.WriteFile(t.getDateAndTime());
+           }
         }
     }   
 }
